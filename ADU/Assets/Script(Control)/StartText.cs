95183c49aed12d19efff1faf883369e8166f4b68
kumakuma503 <kanikanidaisuke@gmail.com>
2022-05-22 22:27:38 +0900

    スタート時の処理追加
    StartText.csを追加。
    
    ●処理内容概略
    戦闘シーン開始時にPlayerMove.csを無効化、またボタンも非表示にする。
    その後画面中央に文字表示(Wave 1)。
    一定時間経過後、文字を変更(START!!)。
    一定時間経過後、文字を非表示にし、最初に無効化したPlayerMove.csとボタンを有効化する。

---
+++ ADU/Assets/Script(Control)/StartText.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI Text;
+    public GameObject button1;
+    public GameObject button2;
+
+    private void Start()
+    {
+        //文字を非表示にする
+        this.gameObject.SetActive(false);
+
+        //ボタンを非表示にする
+        button1.SetActive(false);
+        button2.SetActive(false);
+
+        //0.5秒後に実行
+        Invoke(nameof(DisplayOn), 0.5f);
+
+        //2.5秒後に実行
+        Invoke(nameof(ChangeText), 2.5f);
+
+        //4.5秒後に実行
+        Invoke(nameof(DisplayOff), 4.5f);
+    }
+
+    void DisplayOn()
+    {
+        //オブジェクトを表示する
+        this.gameObject.SetActive(true);
+
+        //スクリプトの無効化（操作をできないようにするため）
+        GameObject playerObj = GameObject.Find("Player");
+        PlayerMove playerMove = playerObj.GetComponent<PlayerMove>();
+        playerMove.enabled = false;
+    }
+
+    void ChangeText()
+    {
+        //文字を変更
+        Text.text = "START!!";
+        //文字色を変更
+        Text.color = Color.red;
+    }
+
+    void DisplayOff()
+    {
+        //文字を表示する
+        this.gameObject.SetActive(false);
+
+        //ボタンを表示にする
+        button1.SetActive(true);
+        button2.SetActive(true);
+
+        //スクリプトの有効化（操作を再開）
+        GameObject playerObj = GameObject.Find("Player");
+        PlayerMove playerMove = playerObj.GetComponent<PlayerMove>();
+        playerMove.enabled = true;
+    }
+}
