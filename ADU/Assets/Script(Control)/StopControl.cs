6026d9e55b3eb9e50d5e45b7c93b1ee507070d1f
stonaprice <103314285+stonaprice@users.noreply.github.com>
2022-06-06 00:09:57 +0900

    中間絶望発表
    ちんこ

--- ADU/Assets/Script(Control)/StopControl.cs
+++ ADU/Assets/Script(Control)/StopControl.cs
@@ -7,24 +7,15 @@ public class StopControl : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI Text;
-    public GameObject button1;
-    public GameObject button2;
-    public GameObject button3;
-
+    public GameObject gameobject;
 
     public void StopMethod()
     {
-        Time.timeScale = 0;
-        this.gameObject.SetActive(false);
+        gameobject.SetActive(false);
+    }
 
-        button1.SetActive(false);
-        button2.SetActive(false);
-        button3.SetActive(false);
-        //sum.SetActive(false);
-
-
-        GameObject playerObj = GameObject.Find("Player");
-        PlayerMove playerMove = playerObj.GetComponent<PlayerMove>();
-        playerMove.enabled = false;
+    public void RestartMethod()
+    {
+        gameobject.SetActive(true);
     }
 }
